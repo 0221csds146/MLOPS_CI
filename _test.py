444af90f9da0a669f4dfb71e5ef6fed9113574ea
Rajeev <kumarr22470@gmail.com>
2025-08-07 23:27:40 +0530

    Error solving2

--- _test.py
+++ _test.py
@@ -1,30 +1,33 @@
 import pytest
 
+# Function to test square
 def square(n):
-    return n*n
+    return n ** 2
 
+# Function to test cube
 def cube(n):
-    return n**3
+    return n ** 3
 
+# Function to test fifth power
 def fifth_power(n):
-    return n**5
+    return n ** 5
 
-#Testing the Square function
+# Testing the square function
 def test_square():
-    assert square(2)==4,"Test Failed: square of 2 should be 4"
-    assert square(3)==9,"Test Failed: square of 3 should be 9"
+    assert square(2) == 4, "Test Failed: Square of 2 should be 4"
+    assert square(3) == 9, "Test Failed: Square of 3 should be 9"
 
-#Testing the cube function
+# Testing the cube function
 def test_cube():
-    assert cube(2)==8,"Test Failed: cube of 2 should be 8"
-    assert cube(3)==27,"Test Failed: cube of 3 should be 27"
+    assert cube(2) == 8, "Test Failed: Cube of 2 should be 8"
+    assert cube(3) == 27, "Test Failed: Cube of 3 should be 27"
 
-#Testing the fifth_power function
+# Testing the fifth power function
 def test_fifth_power():
-    assert fifth_power(2)==32,"Test Failed: fifth_power of 2 should be 32"
-    assert fifth_power(3)==243,"Test Failed: fifth_power of 3 should be 243"
+    assert fifth_power(2) == 32, "Test Failed: Fifth power of 2 should be 32"
+    assert fifth_power(3) == 243, "Test Failed: Fifth power of 3 should be 243"
 
-#Test for invalid input
+# Test for invalid input
 def test_invalid_input():
-    with pytest.raises("TypeError"):
+    with pytest.raises(TypeError):
         square("string")
